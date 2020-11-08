1b6690654ceee8053458105da4e2870c4240ba7c
yeahCH <engel0088@gmail.com>
2020-11-08 11:45:49 +0900

    test: react-ui test initial

--- react-ui/src/App.test.js
+++ react-ui/src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders Contacts title', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const titleElement = screen.getByText(/Contacts/i);
+  expect(titleElement).toBeInTheDocument();
 });
